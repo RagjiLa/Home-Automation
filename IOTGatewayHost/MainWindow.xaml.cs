17dabd628803d2328eaa9486b875a84ed92d0a4a
Ragji <105050656@LOGON.DS.GE.COM>
2016-05-10 14:06:30 +0530

    Added Frontend for application

---
+++ IOTGatewayHost/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IOTGatewayHost
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            WireupTabcontrol(MainContainer, MainContainerHeaders);
+        }
+
+        private static void WireupTabcontrol(TabControl container, ItemsControl headers)
+        {
+            var tabHeaders = new List<MyHeader>();
+            var actionObject = new TabNavigateCommand(container);
+            foreach (TabItem tab in container.Items)
+            {
+                tabHeaders.Add(new MyHeader
+                {
+                    HeaderText = tab.Header.ToString(),
+                    NavigateAction = actionObject,
+                    Index = container.Items.IndexOf(tab)
+                });
+            }
+            headers.ItemsSource = tabHeaders;
+        }
+
+        public class MyHeader
+        {
+            public string HeaderText { get; set; }
+            public int Index { get; set; }
+            public TabNavigateCommand NavigateAction { get; set; }
+        }
+
+        public class TabNavigateCommand : ICommand
+        {
+            private readonly TabControl _target;
+            public bool CanExecute(object parameter)
+            {
+                return true;
+            }
+
+            public event EventHandler CanExecuteChanged;
+
+            public void Execute(object parameter)
+            {
+                var tabIndex = ((MyHeader)parameter).Index;
+                _target.SelectedIndex = tabIndex;
+
+            }
+
+            public TabNavigateCommand(TabControl targetTabControl)
+            {
+                _target = targetTabControl;
+            }
+        }
+    }
+}
